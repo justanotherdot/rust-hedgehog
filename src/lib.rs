9552a5368fb4ec28a075c393c1e75f13fe69e048
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-06-01 21:23:03 +1000

    Teach thunks to memoize their thinks

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,28 @@
 pub mod lazy {
-    pub struct Thunk<T, F: FnOnce() -> T> {
+    #[derive(Copy, Clone)]
+    pub struct Thunk<T: Clone, F: FnOnce() -> T> {
         _think: F,
         _val: Option<T>,
     }
 
-    impl <T, F: FnOnce() -> T> Thunk<T, F> {
+    impl<T: Clone, F: FnOnce() -> T> Thunk<T, F> {
         pub fn new(closure: F) -> Thunk<T, F> {
             Thunk {
                 _think: closure,
-                _val: None
+                _val: None,
             }
         }
 
-        pub fn force(self) -> T {
+        fn think(self) -> T {
+            (self._think)()
+        }
+
+        pub fn force(mut self) -> T {
             match self._val {
-                Some(v) =>
-                    v,
+                Some(v) => v,
                 None => {
-                    let think = self._think;
-                    let rv = think();
-                    //self._val = Some(rv);
+                    let rv = self.think();
+                    self._val = Some(rv.clone());
                     rv
                 }
             }
@@ -34,6 +37,14 @@ mod tests {
     #[test]
     fn it_works() {
         let t = Thunk::new(|| 5);
+        let v = t.force();
+        assert_eq!(v, 5);
+    }
+
+    #[test]
+    fn it_memoizes_values() {
+        let t = Thunk::new(|| 5);
+        t.force();
         let v = t.force();
         assert_eq!(v, 5);
     }
