6804430d49f97ac7f281b0a7b351389df882b1ef
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-06-01 20:21:55 +1000

    Initial Thunk implementation
    I am not sure if this is actually thread safe or not; normally it would
    be given that two context switches across a shared Thunk while it's
    being forced, I believe.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+pub mod lazy {
+    pub struct Thunk<T, F: FnOnce() -> T> {
+        _think: F,
+        _val: Option<T>,
+    }
+
+    impl <T, F: FnOnce() -> T> Thunk<T, F> {
+        pub fn new(closure: F) -> Thunk<T, F> {
+            Thunk {
+                _think: closure,
+                _val: None
+            }
+        }
+
+        pub fn force(self) -> T {
+            match self._val {
+                Some(v) =>
+                    v,
+                None => {
+                    let think = self._think;
+                    let rv = think();
+                    //self._val = Some(rv);
+                    rv
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use lazy::Thunk;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let t = Thunk::new(|| 5);
+        let v = t.force();
+        assert_eq!(v, 5);
     }
 }
