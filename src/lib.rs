72c753ee1c5635468129786588ee1c79d6f8f690
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-06-03 19:24:38 +1000

    Give thunks a lifetime for their closures

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod lazy {
-    //use std::rc::Rc;
+    use std::rc::Rc;
     use std::cell::RefCell;
 
     pub struct Thunk<'a, T> {
@@ -7,7 +7,7 @@ pub mod lazy {
         _think: Box<'a + Fn() -> T>,
     }
 
-    impl <'a, T> Thunk<'a, T> {
+    impl <'a, T: Clone> Thunk<'a, T> {
         pub fn new<F>(clsr: F) -> Thunk<'a, T> where F: 'a + Fn() -> T {
             Thunk {
                 _think: Box::new(clsr),
@@ -15,70 +15,17 @@ pub mod lazy {
             }
         }
 
-        pub fn force(self) -> Option<T> {
+        pub fn force(&self) -> T {
             if self._thunk.borrow().is_none() {
                 let rv = (self._think)();
                 self._thunk.replace(Some(rv));
             }
-            self._thunk.into_inner()
+            self._thunk
+                .clone()
+                .into_inner()
+                .unwrap()
         }
     }
-
-    //pub struct Cell<T> {
-        //pub head: T,
-        //tail: Stream<T>,
-    //}
-
-    //impl <T> Cell<T> {
-        //pub fn new(x: T, tail: Stream<T>) -> Cell<T> {
-            //Cell {
-                //head: x,
-                //tail: tail,
-            //}
-        //}
-    //}
-
-    //pub struct Stream<T> {
-        //_cell: Rc<Thunk<Cell<T>>>
-    //}
-
-    //impl <T> Stream<T> {
-        //// TODO For finite lists.
-        ////pub fn empty() {
-            ////Stream {
-                ////_cell: None
-            ////}
-        ////}
-
-        //pub fn get(self) -> Option<T> {
-            //Rc::try_unwrap(self._cell)
-        //}
-
-        //pub fn new(f: Box<Fn() -> Cell<T>>) -> Stream<T> {
-            //Stream {
-                //_cell: Rc::new(Thunk::new(f))
-            //}
-        //}
-    //}
-
-    //impl <T> Iterator for Stream<T> {
-        //type Item = Rc<Thunk<Cell<T>>>;
-
-        //fn next(&mut self) -> Option<Self::Item> {
-            //match Rc::try_unwrap(self._cell.clone()) {
-                //Ok(thunk) => {
-                    //match thunk.force() {
-                        //Some(cell) => {
-                            //self._cell = cell.tail._cell;
-                            //Some(self._cell.clone())
-                        //},
-                        //None => None
-                    //}
-                //},
-                //_ => None,
-            //}
-        //}
-    //}
 }
 
 #[cfg(test)]
@@ -91,28 +38,20 @@ mod tests {
     fn thunks_defer_application_until_forced() {
         let t = Thunk::new(|| 5);
         let v = t.force();
-        assert_eq!(v, Some(5));
+        assert_eq!(v, 5);
     }
 
     #[test]
     fn thunks_memoize_values() {
         let t = Thunk::new(|| SystemTime::now());
-        let p = t.force().unwrap();
-        let q = p;
+        let p = t.force();
+        let q = t.force();
         assert_eq!(p, q);
     }
 
     //#[test]
-    //fn streams_are_lazy_and_infinite() {
-        //fn ints_from(n: usize) -> Stream<usize> {
-            //Stream::new(Box::new(move || Cell::new(n, ints_from(n+1))))
-        //}
-
-        //let mut strm = ints_from(5);
-        //let x = strm.next();
-        ////assert_eq!(strm.take(5).count(), 5);
-        ////for x in strm.iter().take(5) {
-            ////assert_eq!(x, x); // Not valid!
-        ////}
+    //fn lazy_cells_have_a_head_and_a_tail() {
+        //let cell = Cell::new(13, Stream::empty());
+        //let x:() = cell.head;
     //}
 }
