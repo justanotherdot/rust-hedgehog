ad7e5639055345afbf3e028da52ef4f669abcaf0
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-06-14 15:28:58 +1000

    Rm dead comments

--- src/lazy.rs
+++ src/lazy.rs
@@ -14,8 +14,6 @@ impl<'a, A> Lazy<'a, A>
 where
     A: Clone + 'a,
 {
-    // TODO: We can simplify the ergonomics of this interface by not requiring a closure.
-    // Instead, we can take a value, A, and do the closure ourselves.
     pub fn new(value: A) -> Lazy<'a, A> {
         Lazy {
             closure: Rc::new(move || value.clone()),
@@ -23,8 +21,6 @@ where
         }
     }
 
-    // TODO: We can simplify the ergonomics of this interface by not requiring a closure.
-    // Instead, we can take a value, A, and do the closure ourselves.
     pub fn from_closure<F>(closure: F) -> Lazy<'a, A>
     where
         F: 'a + Fn() -> A,
