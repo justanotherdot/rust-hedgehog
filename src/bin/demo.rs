2dbe938c2765d27dab65c359da9ff39970ab5160
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-07-29 20:11:52 +1000

    Uncurry all functions in gen
    This is missing one helper.

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -4,7 +4,7 @@ use hedgehog::gen;
 use hedgehog::range;
 
 fn main() {
-    gen::print_sample(gen::vec(range::constant(1, 10))(gen::u8(range::constant(
+    gen::print_sample(gen::vec(range::constant(1, 10), gen::u8(range::constant(
         5, 15,
     ))));
 }
